dcfe6620e94c7aebbc412251222053fc87f7d4d2
Eduardo Santos <eduardo@yacows.com.br>
2015-10-19 17:34:40 -0200

    Criação de transações funcional através da interface

--- pagarme/resource.py
+++ pagarme/resource.py
@@ -18,13 +18,17 @@ class AbstractResource(object):
         error_string = e['type'] + ' - ' + e['message']
         raise PagarmeApiError(error_string)
 
-    def create(self):
+    def create(self, callback=None, callback_error=None):
         url = self.BASE_URL
         pagarme_response = requests.post(url, data=self.get_data())
         if pagarme_response.status_code == 200:
             self.handle_response(json.loads(pagarme_response.content))
+            if callback is not None:
+                callback(pagarme_response)
         else:
             self.error(pagarme_response.content)
+            if callback_error is not None:
+                callback_error(pagarme_response)
 
     def get_data(self):
         return self.data
